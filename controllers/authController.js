62de3f7a9c8cd2799a85d4654f59ab88077e0429
Manideep Kakkerla <manideepkakkerla2004@gmail.com>
2026-02-17 21:07:29 +0530

    login working

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,8 @@
 
-import { hash } from "bcrypt";
+import bcrypt from "bcrypt";
 import { User } from "../models/userModel.js";
 import hashPassword from "../services/authServices.js";
+import jwt from "jsonwebtoken";
 export async function registerUser(req,res){
     const {name,email,password}=req.body;
     if(!name || !email || ! password){
@@ -16,4 +17,21 @@ export async function registerUser(req,res){
     } catch (error) {
         res.status(404).send(error.message);
     }
+}
+
+export async function loginUser(req,res){
+    const {email,password}=req.body;
+        if(!email || ! password){   
+        res.status(404).send("fields are empty")
+    }
+    const user= await User.findOne({email});
+        if(!user){  
+        res.status(404).send("user not found")
+    }
+        const isPasswordValid= await bcrypt.compare(password,user.password);
+        if(!isPasswordValid){
+            res.status(404).send("invalid password")
+        }
+        const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"1h"});
+        res.status(200).json({token});  
 }
